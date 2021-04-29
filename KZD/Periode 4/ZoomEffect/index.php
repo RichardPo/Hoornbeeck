53bb867ffd76dab76a2a6fc40852fa5ae8a46bf6
Richard Post <richard.hn.post@gmail.com>
2021-04-29 10:09:53 +0200

    Add KZD and create zoom effect in HTML/CSS

---
+++ KZD/Periode 4/ZoomEffect/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Zoom Effect</title>
+
+        <link href="src/css/style.css?time=<?= time(); ?>" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container center">
+            <div class="block-wrapper center">
+                <div class="block"><div class="block-child" style="background-image: url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.herzindagi.info%2Fimage%2F2020%2FDec%2Ffruits-s.jpg&f=1&nofb=1);"></div></div>
+                <div class="block"><div class="block-child" style="background-image: url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Felinkwijk.nl%2Fwp-content%2Fuploads%2F2016%2F08%2Fvoetbal-actie.jpg&f=1&nofb=1);"></div></div>
+                <div class="block"><div class="block-child" style="background-image: url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.basketbal.vlaanderen%2Fsrc%2FFrontend%2FFiles%2Fblog%2Fimages%2Fsource%2Fbasketbal-door-de-ogen-van-een-startende-ref-nl.jpg&f=1&nofb=1);"></div></div>
+            </div>
+        </div>
+    </body>
+</html>
